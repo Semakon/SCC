a3b8ebdc32f8bfae49edffd70c2d5b3a38408b7e
Martijn <m.p.devries@studente.utwente.nl>
2020-10-27 16:51:14 +0100

    implemented key_gen

--- assignment_2/van_dijk.py
+++ assignment_2/van_dijk.py
@@ -1,3 +1,6 @@
+import random
+
+
 class VanDijk:
 
     def __init__(self, eta, gamma, rho, tau):
@@ -6,9 +9,25 @@ class VanDijk:
         self.rho = rho
         self.tau = tau
 
-    def key_gen(self, labda):
+    def key_gen(self):
+        # Generate secret key as eta-bit odd integer
+        p = random.randint(2**(self.eta - 1), (2**self.eta) - 1)
+        while p % 2 == 0:
+            p = random.randint(2**(self.eta - 1), (2**self.eta) - 1)
 
-        pass
+        # Generate public key
+        q0 = random.randrange(0, int((2**self.gamma) / p))
+        x0 = p * q0
+        pk = [x0]
+        for i in range(1, self.tau + 1):
+            xi = self.__D__(p)
+            while x0 <= xi:
+                # xi must be strictly smaller than x0
+                xi = self.__D__(p)
+            pk.append(xi)
+        assert len(pk) == self.tau + 1
+
+        return p, pk
 
     def enc(self, pk, m):
         pass
@@ -18,3 +37,12 @@ class VanDijk:
 
     def dec(self, sk, c):
         pass
+
+    def __D__(self, p):
+        q = random.randrange(0, int((2**self.gamma) / p))
+        r = random.randrange(-(2**self.rho) + 1, 2**self.rho)
+        return (p * q) + r
+
+
+test = VanDijk(100, 1000, 10, 10)
+print(test.key_gen())
