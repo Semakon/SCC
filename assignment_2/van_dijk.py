23c3842ca01ba554928dda555ef2a0464bdfbc1e
Martijn <m.p.devries@studente.utwente.nl>
2020-10-27 16:03:43 +0100

    Initial commit

---
+++ assignment_2/van_dijk.py
@@ -0,0 +1,20 @@
+class VanDijk:
+
+    def __init__(self, eta, gamma, rho, tau):
+        self.eta = eta
+        self.gamma = gamma
+        self.rho = rho
+        self.tau = tau
+
+    def key_gen(self, labda):
+
+        pass
+
+    def enc(self, pk, m):
+        pass
+
+    def eval(self, pk, P, c):
+        pass
+
+    def dec(self, sk, c):
+        pass
